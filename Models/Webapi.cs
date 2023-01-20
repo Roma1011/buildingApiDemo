3e30e3e1e4d0f7ef02c285487975a1f0805fc1d6
Roma <rkekua9@gmail.com>
2023-01-20 05:40:05 -0800

    Modified modified Dto

--- Models/Webapi.cs
+++ Models/Webapi.cs
@@ -4,8 +4,16 @@ namespace webapi.Models
 {
     public class Webapi
     {
-        [Required]
         public int Id { get; set; }
         public string? name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
     }
 }
